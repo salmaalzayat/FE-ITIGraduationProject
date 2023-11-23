2ff9527615151cb5f6014c80e2fc2ccb6a025c9b
salma alzayat <salmaalzayat888@gmail.com>
2023-11-23 16:26:52 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from './services/authService.service';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,22 @@ import { AuthenticationService } from './services/authService.service';
 })
 export class AppComponent implements OnInit {
   title = 'graduation-project';
-  constructor( private authService: AuthenticationService ){}
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     if (localStorage.getItem('token')) {
       this.authService.isLoggedIn$.next(true);
     }
+
+    // scroll to the top when the route changes
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      window.scrollTo(0, 0);
+    });
   }
 }
