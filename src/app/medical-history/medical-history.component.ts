c97119ece7a15a95b930d80ab8c834847839b6a5
salma alzayat <salmaalzayat888@gmail.com>
2023-11-29 12:16:49 +0200

    update

--- src/app/medical-history/medical-history.component.ts
+++ src/app/medical-history/medical-history.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authService.service';
  import { GetMedicalHistoryByPhoneDto } from '../Types/GetMedicalHistoryByPhoneDto';
 import { patiantMedicalHistory } from '../services/patientMedicalHistory.service';
+import { LoadingService } from 'src/app/services/loading.service';
+import { tap } from 'rxjs/operators'; 
 
 @Component({
   selector: 'app-medical-history',
@@ -16,7 +18,8 @@ export class MedicalHistoryComponent implements OnInit {
   errorLoadingData: boolean = false;
 
   constructor(
-    private patientMedicalHistory: patiantMedicalHistory
+    private patientMedicalHistory: patiantMedicalHistory,
+    private loadingService : LoadingService
   ) {}
 
   ngOnInit(): void {
@@ -29,7 +32,11 @@ export class MedicalHistoryComponent implements OnInit {
       this.userData = JSON.parse(storedUserData);
       this.phoneNumber = this.userData.phoneNumber;
       console.log(this.phoneNumber);
-      this.patientMedicalHistory.getMedicalHistory(this.phoneNumber).subscribe({
+      this.patientMedicalHistory.getMedicalHistory(this.phoneNumber).pipe(
+        tap(() => {
+          this.loadingService.setLoading(true); 
+        })
+      ).subscribe({
         next: (medicalHistory) => {
           this.MedicalHistoryDto = medicalHistory;
           console.log(this.MedicalHistoryDto);
@@ -37,6 +44,9 @@ export class MedicalHistoryComponent implements OnInit {
         error: (error) => {
           console.error('Calling API failed', error);
           this.errorLoadingData = true;
+        },
+        complete: () => {
+          this.loadingService.setLoading(false); 
         },
       });
     }
